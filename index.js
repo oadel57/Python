372a391f42017dd2d40e785d53c8725b33998f27
oadel57 <oadel57@gmail.com>
2025-04-02 22:16:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,164 +1,40 @@
-//Current line
-var CurrentId = undefined;
+// Caesar Cipher in JavaScript
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-var inputValues = [];
-
-const letters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"];
-const inputPrompts = [
-  "How many symbols would you like?",
-  "How many numbers would you like?",
-];
-
-//Click Run
-$(document).ready(function () {
-  $("#run-button").click(function () {
-    inputValues = [];
-    $("#Content").empty();
-    NewLine("Welcome to the PyPassword Generator!", false);
-    NewLine("How many letters would you like in your password?", true);
-  });
-});
-
-//Enter button
-$(document).on("keydown", function (e) {
-  var x = event.which || event.keyCode;
-  if (x === 13 || x == 13) {
-    var consoleLine = $("#" + CurrentId + " input").val();
-    inputValues.push({ id: CurrentId, val: consoleLine });
-
-    if (inputValues.length > inputPrompts.length) {
-      let password_list = [];
-      for (let i = 0; i < Number(inputValues[0].val); i++) {
-        console.log(i);
-        password_list.push(letters[Math.floor(Math.random() * letters.length)]);
-      }
-
-      for (let i = 0; i < Number(inputValues[1].val); i++) {
-        password_list.push(symbols[Math.floor(Math.random() * symbols.length)]);
-      }
-      for (let i = 0; i < Number(inputValues[2].val); i++) {
-        password_list.push(numbers[Math.floor(Math.random() * numbers.length)]);
-      }
-      NewLine("", false);
-      const shuffledPasswordList = shuffleArray(password_list);
-      NewLine("", false);
-
-      let password = "";
-      for (const char in shuffledPasswordList) {
-        password += shuffledPasswordList[char];
-      }
-      NewLine("Your password is: " + password, false);
-
-      $(".console-carrot").remove();
-      return;
+function caesar(originalText, shiftAmount, encodeOrDecode) {
+    let outputText = "";
+    
+    // Reverse shift if decoding
+    if (encodeOrDecode === "decode") {
+        shiftAmount *= -1;
     }
-    $(".console-carrot").remove();
-    NewLine(inputPrompts[inputValues.length - 1], true);
-  }
-});
-$(document).on("keydown", function (e) {
-  var x = event.which || event.keyCode;
-  var line = $("#" + CurrentId + " input");
-  var length = line.val().length;
-  if (x != 8) {
-    line.attr("size", 1 + length);
-  } else {
-    line.attr("size", length * 0.95);
-  }
-  if (length === 0) {
-    $("#" + CurrentId + " input").attr("size", "1");
-  }
-});
-$(document).on("click", function (e) {
-  $("#" + CurrentId + " input").focus();
-});
-
-//New line
-function NewLine(text, isPrompt) {
-  $(".console-carrot").remove();
-  if (CurrentId !== undefined) {
-    $("#" + CurrentId + " input").prop("disabled", true);
-  }
-  CurrentId = "consoleInput-" + GenerateId();
-
-  if (isPrompt) {
-    $("#Content").append("<div>" + text + "</div>");
-    $("#Content").append(
-      '<div id="' +
-        CurrentId +
-        '">' +
-        '<input autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" type="text" class="terminal-input" /><div class="console-carrot"></div></div>'
-    );
-    $("#" + CurrentId + " input").focus();
-    $("#" + CurrentId + " input").attr("size", "1");
-  } else {
-    $("#Content").append('<div id="' + CurrentId + '">' + text + "</div>");
-  }
+    
+    for (let letter of originalText) {
+        if (alphabet.includes(letter)) {
+            let shiftedPosition = alphabet.indexOf(letter) + shiftAmount;
+            shiftedPosition = (shiftedPosition + alphabet.length) % alphabet.length;
+            outputText += alphabet[shiftedPosition];
+        } else {
+            outputText += letter; // Keep non-alphabet characters as is
+        }
+    }
+    
+    console.log(`Here is the ${encodeOrDecode}d result: ${outputText}`);
 }
 
-function GenerateId() {
-  return Math.random().toString(16).slice(2);
+function startCipher() {
+    let again = "yes";
+    while (again === "yes") {
+        let direction = prompt("Type 'encode' to encrypt, type 'decode' to decrypt:").toLowerCase();
+        let text = prompt("Type your message:").toLowerCase();
+        let shift = parseInt(prompt("Type the shift number:"), 10);
+
+        caesar(text, shift, direction);
+        
+        again = prompt("Type 'yes' if you want to go again. Otherwise, type 'no'").toLowerCase();
+    }
+    console.log("Game Over!");
 }
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+// Start the program
+startCipher();
